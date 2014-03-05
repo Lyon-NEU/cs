e0b3162fa2ee8dfc5cce62ee986641e64e80e905
Lyon-NEU <wanlplab@gmail.com>
2014-03-06 00:24:32 +0800

    go go go!

--- src/hmm/HMM.java
+++ src/hmm/HMM.java
@@ -4,13 +4,22 @@
  */
 package hmm;
 public class HMM {
-	private int M;   
-	private int N;
-	private int transmit[][]=new int[N][N];
-	private int generate[][]=new int[N][M];
+	private int M;     //number of observation symbols
+	private int N;     //number of hidden states
+	private int [][]transmit;  //transition probability of going from  state i at time t to state j at time t+1 
+	private int [][]generate;   // probability of observation symbol k at i
 	public HMM(int m,int n){
 		this.M=m;
 		this.N=n;
+	}
+	public double forward(){
+		return 0;
+	}
+	public double backward(){
+		return 0;
+	}
+	public double viterbi(){
+		return 0;
 	}
 	public static void main(String[] args){
 		System.out.println("----------My HMM-------------");
